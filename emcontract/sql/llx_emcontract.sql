118f3d2c78d5f5c2eb77689e052ea678f20136fe
Patrick Delcroix <pmpdelcroix@gmail.com>
2015-03-11 21:49:10 +0100

    new fields

--- emcontract/sql/llx_emcontract.sql
+++ emcontract/sql/llx_emcontract.sql
@@ -43,16 +43,15 @@ rowid                 	integer NOT NULL AUTO_INCREMENT,
 entity	              	integer DEFAULT 1 NOT NULL,		-- multi company id
 datec                 	DATETIME NOT NULL,
 datem		      	TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-type_contract         	integer NOT NULL,                  
+type_contract         	integer NOT NULL,      -- 0- admin, 1- CDI, 2- CDD, 3- apprentissage , 4- stage ,,          
 description           	VARCHAR( 255 ) NOT NULL,
 fk_user_author        	integer ,
 fk_user_modif         	integer ,
-weekly_hours	      	integer NOT NULL, -- operand 0 |
-modulation_period      	integer DEFAULT 0, -- operand 1 | 0 - one week, 1- one month, 2- two month ...
-working_days           	integer DEFAULT 31, -- operand 2 | (2^0)=1- monday, (2^1)=2- tuesday, (2^2)=4- Wednesday ... ex M+T+W+T+F=1+2+4+8+16=31, 
-normal_rate_days     	integer DEFAULT 31, -- operand 3 |  all other worink day are regarded as days with an overrate
-daily_hours	      	integer Default 8, -- operand 4 | informative, could be used for the timesheet
-base_rate		integer NOT NULL, -- operand 5 |
+weekly_hours	      	integer NOT NULL, -- operand 1 |
+modulation_period      	integer DEFAULT 0, -- operand 2 | 0 - one week, 1- one month, 2- two month ...
+working_days           	integer DEFAULT 31, -- operand 3 | (2^0)=1- monday, (2^1)=2- tuesday, (2^2)=4- Wednesday ... ex M+T+W+T+F=1+2+4+8+16=31, 
+normal_rate_days     	integer DEFAULT 31, -- operand 4 |  all other worink day are regarded as days with an overrate
+daily_hours	      	integer Default 8, -- operand 5 | informative, could be used for the timesheet
 night_hours_start   	TIME DEFAULT "21:00:00",-- operand 6 |
 night_rate	      	integer default 1.5,	-- operand 7 |
 night_hours_stop	TIME DEFAULT "06:00:00",-- operand 8 |
@@ -117,6 +116,7 @@ date_start_contract   date NOT NULL,
 date_end_contract     date NULL,
 fk_user_author        integer,
 fk_user_modif         integer, 
+base_rate		integer NOT NULL, -- operand 0 |
 PRIMARY KEY (rowid),
 FOREIGN KEY (fk_emcontract_type ) REFERENCES llx_emcontract_type(rowid),
 FOREIGN KEY (fk_user_author) REFERENCES llx_user(rowid),
